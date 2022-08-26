f31ec62a3f12af70de0743693ed20a9e40bdeb35
Rachel <marcotter25@yahoo.com>
2022-08-25 19:11:48 -0700

    javascript buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,21 +14,45 @@ fetch('https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=8
 	.catch(err => console.error(err));
 
 let movies = []
+let genres = {}
 
 buttonClick = () => {
-    $('#genre').change(function(event) {
+    $('.genreId').change(function(event) {
         if ($(event.target).is(':checked')) {
             movies.push($(event.target).attr('id'))
             console.log(movies)
         } else {
             movies = movies.filter(item => item !== $(event.target).attr('id'))
             console.log(movies)
-        }
+        };
+        
+      
     });
 };   
 
+let movieChoice = () => {
+if (movies[0] === 'comedy') {
+    genres.push('35')
+    console.log(genres)
+
+} else if (movieChoice === horror) {
+    genres.push('36')
+    console.log(genres)
+
+} else if (movieChoice === action) {
+    genres.push('28')
+
+} else if (movieChoice === drama) {
+    genres.push('18')
+
+} else {
+    genres.push('99')
+};
+}
 
 buttonClick();
+
+
 
 
 
